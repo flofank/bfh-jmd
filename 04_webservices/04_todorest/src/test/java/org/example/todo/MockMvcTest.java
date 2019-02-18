c305396a65aa4e0546852906b2a1dd6b174a8d23
flofank <flofank@gmx.ch>
2019-02-18 10:11:52 +0100

    Now also using RestAssured and TestRestTemplate.

--- 04_webservices/04_todorest/src/test/java/org/example/todo/MockMvcTest.java
+++ 04_webservices/04_todorest/src/test/java/org/example/todo/MockMvcTest.java
@@ -2,17 +2,23 @@ package org.example.todo;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -20,12 +26,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class MockMvcTest {
 
 	@Autowired
 	private MockMvc mockMvc;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @LocalServerPort
+  int port;
 
 	@Test
 	public void exampleTest() throws Exception {
@@ -39,6 +51,13 @@ public class MockMvcTest {
 	}
 
 
+  @Test
+  public void exampleTest2() {
+    Todo t = restTemplate.getForObject("/todos/1", Todo.class);
+    assertThat(t.getTitle()).isEqualTo("Testing");
+  }
+
+
   public static String asJsonString(final Object obj) {
     try {
       final ObjectMapper mapper = new ObjectMapper();
@@ -47,5 +66,16 @@ public class MockMvcTest {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  @Before
+  public void init() {
+    RestAssured.port = port;
+    RestAssured.basePath = "/todos";
+  }
+
+  @Test
+  public void exampleTest3() {
+    given().when().get("/1").then().body(containsString("Testing"));
   }
 }
